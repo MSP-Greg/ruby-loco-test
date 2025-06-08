4e134c1bdaf03d091ede30cce8c5d8ed8e6f0ea1
MSP-Greg <Greg.mpls@gmail.com>
2025-06-07 20:40:20 -0500

    Update UpdateVS.ps1

--- UpdateVS.ps1
+++ UpdateVS.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest https://aka.ms/vs/17/release/vs_Enterprise.exe -OutFile C:/vs_Enterprise.exe
-C:/vs_Enterprise.exe update --quiet --norestart --wait --installpath "C:/Program Files/Microsoft Visual Studio/2022/Enterprise"
+Start-Process -Wait -FilePath C:/vs_Enterprise.exe -ArgumentList "update --quiet --norestart --installpath ""C:\Program Files\Microsoft Visual Studio\2022\Enterprise"""

622b5389fb3ecc2144ead4e86d58ec174433d0c1
MSP-Greg <Greg.mpls@gmail.com>
2025-06-08 09:15:43 -0500

    Update UpdateVS.ps1

--- UpdateVS.ps1
+++ UpdateVS.ps1
@@ -1,3 +1,3 @@
-$argList = ('/c', 'C:/vs_Enterprise.exe', 'update', '--quiet', '--norestart', '--nocache', 'channelUri', 'https://aka.ms/vs/17/release/channel', '--channelId', 'VisualStudio.17.release','--installpath', '"C:/Program Files/Microsoft Visual Studio/2022/Enterprise"')
+$argList = ('/c', 'C:/vs_Enterprise.exe', 'install', '--quiet', '--norestart', '--nocache', 'channelUri', 'https://aka.ms/vs/17/release/channel', '--channelId', 'VisualStudio.17.release','--installpath', '"C:/Program Files/Microsoft Visual Studio/2022/Enterprise"')
 $process = Start-Process -FilePath cmd.exe  -ArgumentList $argList -Wait -PassThru -NoNewWindow
 echo $process.ExitCode

e8ef89f85cd3d21d04bd2ff84cc7e9813abde3d9
MSP-Greg <Greg.mpls@gmail.com>
2025-06-08 08:41:32 -0500

    Update UpdateVS.ps1

--- UpdateVS.ps1
+++ UpdateVS.ps1
@@ -1,4 +1,5 @@
 Invoke-WebRequest https://aka.ms/vs/17/release/vs_Enterprise.exe -OutFile C:/vs_Enterprise.exe
+C:/vs_Enterprise.exe --update --quiet --wait
 $argList = ('/c', 'C:/vs_Enterprise.exe', 'update', '--quiet', '--norestart', '--nocache', '--installpath', '"C:/Program Files/Microsoft Visual Studio/2022/Enterprise"')
 $process = Start-Process -FilePath cmd.exe  -ArgumentList $argList -Wait -PassThru -NoNewWindow
 echo $process.ExitCode
